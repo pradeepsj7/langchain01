11a5ed15baa56714ed704414289b83d0e377b69f
Pradeep <pradeepsj7@gmail.com>
2023-08-11 09:41:32 +0530

    Prompt template

--- demo.js
+++ demo.js
@@ -1,12 +1,36 @@
 import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
+import { LLMChain } from "langchain/chains";
+
+const template =
+	"What would be good company name for the company that make {product}?";
+
+const promptTemplate = new PromptTemplate({
+	template: template,
+	inputVariables: ["product"],
+});
 
 const model = new OpenAI({
-	openAIApiKey: "0j1tT5CfHWD0eqCG0GHxT3BlbkFJXFd11HPjqjEPRnbCH3UF",
+	openAIApiKey: "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",
 	temperature: 0.9,
 });
 
-const res = await model.call(
-	"What would be good company name for the company that make Agricultural products?"
-);
+const chain = new LLMChain({
+	llm: model,
+	prompt: promptTemplate,
+});
+const res = await chain.call({
+	product: "Agriculture products",
+});
 
 console.log(res);
+
+//Formatted code
+// const formattedPrompt = await promptTemplates.format({
+// 	product: "Agriculture product",
+// });
+// console.log(formattedPrompt);
+
+// const res = await model.call(
+// 	"What would be good company name for the company that make Agricultural products?"
+// );
